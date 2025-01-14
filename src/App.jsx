2c24414cdffcc19df927ad7309459642d9034c5e
AdamXie3 <adamxie95@gmail.com>
2025-01-15 00:11:42 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BudgetCalculator from './pages/BudgetCalculator';
 import Home from './pages/Home';
+import Lessons from './pages/Lessons';
+import BudgetCalculator from './pages/BudgetCalculator';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <h1>Welcome to the Finwei Budget App</h1>
         <Routes>
-          <Route path="/" element={<BudgetCalculator />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/calculator" element={<BudgetCalculator />} />
         </Routes>
       </div>
     </Router>
