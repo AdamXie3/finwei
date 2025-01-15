c429a335d1f27de64224bb9a0a39bb5561b9960a
AdamXie3 <adamxie95@gmail.com>
2025-01-15 11:33:44 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,20 @@
-import React, { useState } from 'react';
-import { AppBar, Tabs, Tab, Typography, Box } from '@mui/material';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Lessons from './pages/Lessons';
-import BudgetCalculator from './pages/BudgetCalculator'; 
+import BudgetCalculator from './pages/BudgetCalculator';
 
 const App = () => {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const renderTabContent = () => {
-    switch (value) {
-      case 0:
-        return <Home />;
-      case 1:
-        return <Lessons />;
-      case 2:
-        return <BudgetCalculator />;
-      default:
-        return <Home />;
-    }
-  };
-
   return (
-    <Box sx={{ width: '100%' }}>
-      <AppBar position="static">
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Home" />
-          <Tab label="Lessons" />
-          <Tab label="Budget Calculator" />
-        </Tabs>
-      </AppBar>
-      <Box sx={{ p: 3 }}>
-        {renderTabContent()}
-      </Box>
-    </Box>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/calculator" element={<BudgetCalculator />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
